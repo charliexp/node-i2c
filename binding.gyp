d2c9a387a770ee3e34294dfd2ad0d995960d2c0f
Jim Heising <jheising@redfoundry.com>
2013-12-21 13:15:22 -0800

    Made fixes to work on pcDuino

--- binding.gyp
+++ binding.gyp
@@ -2,7 +2,7 @@
   "targets": [
     {
       "target_name": "i2c",
-      "sources": [ "src/i2c.cc" ]
+      "sources": [ "src/i2c.cc", "src/Wire.cpp", "src/Stream.cpp", "src/Print.cpp" ]
     }
   ]
 }
--- src/i2c.cc
+++ src/i2c.cc
@@ -8,31 +8,34 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include "i2c-dev.h"
+#include "Arduino.h"
+#include "Wire.h"
 
 using namespace v8;
-int fd;
+
+TwoWire* wire;
+
+//int fd;
 int8_t addr;
 
-void setAddress(int8_t addr) {
+/*void setAddress(int8_t addr) {
   int result = ioctl(fd, I2C_SLAVE_FORCE, addr);
   if (result == -1) {
     ThrowException(
       Exception::TypeError(String::New("Failed to set address"))
     );
   }
-}
+}*/
 
 Handle<Value> SetAddress(const Arguments& args) {
   HandleScope scope;
 
   addr = args[0]->Int32Value();
-  setAddress(addr);
 
   return scope.Close(Undefined());
 }
 
-Handle<Value> Scan(const Arguments& args) {
+/*Handle<Value> Scan(const Arguments& args) {
   HandleScope scope;
 
   int i, res;
@@ -41,10 +44,11 @@ Handle<Value> Scan(const Arguments& args) {
   Local<Value> err = Local<Value>::New(Null());
 
   for (i = 0; i < 128; i++) {
-    setAddress(i);
+
     if ((i >= 0x30 && i <= 0x37) || (i >= 0x50 && i <= 0x5F)) {
-      res = i2c_smbus_read_byte(fd);
-    } else { 
+      res = wire.requestFrom(i, 1);
+    } else {
+      res = wire.write
       res = i2c_smbus_write_quick(fd, I2C_SMBUS_WRITE);
     }
     if (res >= 0) {
@@ -60,28 +64,30 @@ Handle<Value> Scan(const Arguments& args) {
   callback->Call(Context::GetCurrent()->Global(), argc, argv);
 
   return scope.Close(results);
-}
+}*/
 
 Handle<Value> Close(const Arguments& args) {
-  HandleScope scope;
+HandleScope scope;
 
-  if (fd > 0) {
-    close(fd);
-  }
+	if(wire)
+	{
+		delete wire;
+		wire = NULL;
+	}
+
   return scope.Close(Undefined());
 }
 
 Handle<Value> Open(const Arguments& args) {
   HandleScope scope;
 
-  String::Utf8Value device(args[0]);
+  //v8::String::Utf8Value device(args[0]);
+
+  wire = new TwoWire();
 
   Local<Value> err = Local<Value>::New(Null());
 
-  fd = open(*device, O_RDWR);
-  if (fd == -1) {
-    err = Exception::Error(String::New("Failed to open I2C device"));
-  }
+  wire->begin();
 
   if (args[1]->IsFunction()) {
     const unsigned argc = 1;
@@ -94,18 +100,39 @@ Handle<Value> Open(const Arguments& args) {
   return scope.Close(Undefined());
 }
 
+int waitForBytes(int byteCount)
+{
+	int timeout = 1000 * byteCount;
+
+	while(wire->available() < byteCount)
+	{
+		timeout--;
+
+		if(timeout <= 0)
+		{
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
 Handle<Value> ReadByte(const Arguments& args) {
   HandleScope scope;
-  
-  Local<Value> data; 
+
+  Local<Value> data;
   Local<Value> err = Local<Value>::New(Null());
 
-  int8_t res = i2c_smbus_read_byte(fd);
+  wire->requestFrom(addr, 1);
 
-  if (res == -1) { 
-    err = Exception::Error(String::New("Cannot read device"));
-  } else {
-    data = Integer::New(res);
+  if(waitForBytes(1) == -1)
+  {
+  	err = Exception::Error(v8::String::New("Cannot read device"));
+  }
+  else
+  {
+  	int res = wire->read();
+  	data = Integer::New(res);
   }
 
   if (args[0]->IsFunction()) {
@@ -121,21 +148,36 @@ Handle<Value> ReadByte(const Arguments& args) {
 Handle<Value> ReadBlock(const Arguments& args) {
   HandleScope scope;
 
-  int8_t cmd = args[0]->Int32Value();
-  int32_t len = args[1]->Int32Value();
-  uint8_t data[len]; 
+  uint8_t cmd = args[0]->Int32Value();
+  uint32_t len = args[1]->Int32Value();
+  uint8_t data[len];
   Local<Value> err = Local<Value>::New(Null());
   node::Buffer *buffer =  node::Buffer::New(len);
 
   Local<Object> globalObj = Context::GetCurrent()->Global();
-  Local<Function> bufferConstructor = Local<Function>::Cast(globalObj->Get(String::New("Buffer")));
+  Local<Function> bufferConstructor = Local<Function>::Cast(globalObj->Get(v8::String::New("Buffer")));
   Handle<Value> constructorArgs[3] = { buffer->handle_, v8::Integer::New(len), v8::Integer::New(0) };
   Local<Object> actualBuffer = bufferConstructor->NewInstance(3, constructorArgs);
 
-  while (fd > 0) {
-    if (i2c_smbus_read_i2c_block_data(fd, cmd, len, data) != len) {
-      err = Exception::Error(String::New("Error reading length of bytes"));
-    }
+  wire->beginTransmission(addr);
+    wire->write(cmd);
+    wire->endTransmission();
+
+while (wire) {
+
+  wire->requestFrom(addr, len);
+
+  	if(waitForBytes(len) == -1)
+	{
+		err = Exception::Error(v8::String::New("Cannot read device"));
+	}
+	else
+	{
+		for(int index = 0; index < len; index++)
+        {
+        	data[index] = wire->read();
+        }
+	}
 
     memcpy(node::Buffer::Data(buffer), data, len);
 
@@ -145,7 +187,7 @@ Handle<Value> ReadBlock(const Arguments& args) {
       Local<Value> argv[argc] = { err, actualBuffer };
       callback->Call(Context::GetCurrent()->Global(), argc, argv);
     }
- 
+
     if (args[2]->IsNumber()) {
       int32_t delay = args[2]->Int32Value();
       usleep(delay * 1000);
@@ -160,12 +202,12 @@ Handle<Value> ReadBlock(const Arguments& args) {
 Handle<Value> WriteByte(const Arguments& args) {
   HandleScope scope;
 
-  int8_t byte = args[0]->Int32Value();
+  uint8_t byte = args[0]->Int32Value();
   Local<Value> err = Local<Value>::New(Null());
 
-  if (i2c_smbus_write_byte(fd, byte) == -1) {
-    err = Exception::Error(String::New("Cannot write to device"));
-  }
+  wire->beginTransmission(addr);
+  wire->write(byte);
+  wire->endTransmission();
 
   if (args[1]->IsFunction()) {
     const unsigned argc = 1;
@@ -183,15 +225,16 @@ Handle<Value> WriteBlock(const Arguments& args) {
 
   Local<Value> buffer = args[1];
 
-  int8_t cmd = args[0]->Int32Value();
+  uint8_t cmd = args[0]->Int32Value();
   int   len = node::Buffer::Length(buffer->ToObject());
-  char* data = node::Buffer::Data(buffer->ToObject());
+  uint8_t* data = (uint8_t*)node::Buffer::Data(buffer->ToObject());
 
   Local<Value> err = Local<Value>::New(Null());
 
-  if (i2c_smbus_write_i2c_block_data(fd, cmd, len, (unsigned char*) data) == -1) {
-    err = Exception::Error(String::New("Cannot write to device"));
-  }
+  wire->beginTransmission(addr);
+  wire->write(cmd);
+  wire->write(data, len);
+  wire->endTransmission();
 
   if (args[2]->IsFunction()) {
     const unsigned argc = 1;
@@ -204,14 +247,14 @@ Handle<Value> WriteBlock(const Arguments& args) {
   return scope.Close(Undefined());
 }
 
-Handle<Value> WriteWord(const Arguments& args) {
+/*Handle<Value> WriteWord(const Arguments& args) {
   HandleScope scope;
-  
+
   int8_t cmd = args[0]->Int32Value();
   int16_t word = args[1]->Int32Value();
 
   Local<Value> err = Local<Value>::New(Null());
-  
+
   if (i2c_smbus_write_word_data(fd, cmd, word) == -1) {
     err = Exception::Error(String::New("Cannot write to device"));
   }
@@ -225,31 +268,34 @@ Handle<Value> WriteWord(const Arguments& args) {
   }
 
   return scope.Close(Undefined());
-}
+}*/
 
 void Init(Handle<Object> target) {
-  target->Set(String::NewSymbol("scan"),
-    FunctionTemplate::New(Scan)->GetFunction());
 
-  target->Set(String::NewSymbol("setAddress"),
+	wire = NULL;
+
+  //target->Set(v8::String::NewSymbol("scan"),
+  //  FunctionTemplate::New(Scan)->GetFunction());
+
+  target->Set(v8::String::NewSymbol("setAddress"),
     FunctionTemplate::New(SetAddress)->GetFunction());
 
-  target->Set(String::NewSymbol("open"),
+  target->Set(v8::String::NewSymbol("open"),
     FunctionTemplate::New(Open)->GetFunction());
 
-  target->Set(String::NewSymbol("close"),
+  target->Set(v8::String::NewSymbol("close"),
     FunctionTemplate::New(Close)->GetFunction());
 
-  target->Set(String::NewSymbol("writeByte"),
+  target->Set(v8::String::NewSymbol("writeByte"),
       FunctionTemplate::New(WriteByte)->GetFunction());
 
-  target->Set(String::NewSymbol("writeBlock"),
+  target->Set(v8::String::NewSymbol("writeBlock"),
       FunctionTemplate::New(WriteBlock)->GetFunction());
 
-  target->Set(String::NewSymbol("readByte"),
+  target->Set(v8::String::NewSymbol("readByte"),
     FunctionTemplate::New(ReadByte)->GetFunction());
 
-  target->Set(String::NewSymbol("readBlock"),
+  target->Set(v8::String::NewSymbol("readBlock"),
     FunctionTemplate::New(ReadBlock)->GetFunction());
 
 }
